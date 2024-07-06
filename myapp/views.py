11501ffb47de098fac4ae9e4e3e684b3de9ea902
Imani Ndolo <imanindolo77@gmail.com>
2024-07-06 08:05:04 +0300

    Configured admin and other commits

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 ''' def index(request):  
@@ -13,7 +14,33 @@ from django.http import HttpResponse
  '''
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our services are timely and quality is priority.  You will never regret working with us'
+    feature1.is_true = True
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Top Quality Products'
+    feature2.details = 'When it comes to quaily, you don\'t have to gamble. I provide my best skills.'
+    feature2.is_true = True
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Always Available'
+    feature3.details = 'Customer support cannot be compromised. I provide full support throughout the project'
+    feature3.is_true = False
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Support Services'
+    feature4.details = 'I offer support services like API integration, website hosting, database management,.. etc.'
+    feature4.is_true = True
+    
+    features = [feature1, feature2, feature3, feature4]
+
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     text = request.GET['text']
